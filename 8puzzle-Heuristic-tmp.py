b5b1c2fefab411505df6c83ffe908a480d05fb94
Abhijeetsingh <abhijeetsingh@MacBook-Air.local>
2023-02-08 14:06:11 +0530

    Some Update

--- 8puzzle-Heuristic-tmp.py
+++ 8puzzle-Heuristic-tmp.py
@@ -5,6 +5,15 @@ def ham(ls1,ls2):
             dis+=1
     return dis
 
+def getInvCount(ls):
+    blank = 0
+    inv = 0
+    for i in range(len(ls)):
+        for j in range(i+1, len(ls)):
+            if (ls[j] != blank and ls[i] != blank and ls[i] > ls[j]):
+                inv += 1
+    return inv%2==0
+
 def swap(move,curVal,ind):
     if (move == "up"):
         nextInd = ind-3
@@ -33,12 +42,13 @@ def best(cur,LastMove):
     }
 
     ind0 = cur.index(0)
+    
 
     possibleMoves = []
-    possibleMoves.append("up" if ind0 in moves["up"] else "-")
-    possibleMoves.append("down" if ind0 in moves["down"] else "-")
-    possibleMoves.append("left" if ind0 in moves["left"] else "-")
-    possibleMoves.append("right" if ind0 in moves["right"] else "-")
+    possibleMoves.append("a_up" if ind0 in moves["up"] else "-")
+    possibleMoves.append("b_down" if ind0 in moves["down"] else "-")
+    possibleMoves.append("c_left" if ind0 in moves["left"] else "-")
+    possibleMoves.append("d_right" if ind0 in moves["right"] else "-")
 
     print(f"possibleMoves: {possibleMoves}")
 
@@ -46,75 +56,85 @@ def best(cur,LastMove):
 
     print("Position of '0' : ", ind0)
     print("CUR in best : ", cur)
-    if "up" in possibleMoves:
+    if "a_up" in possibleMoves:
         copyCurUp = cur.copy()
         copyCurUp = swap("up",copyCurUp,ind0)
         print("Possible Move Up: ")
         printLs(copyCurUp)
-        # h = ham(initial,copyCurUp)
+        h = ham(initial,copyCurUp)
         g = ham(copyCurUp,goal)
-        f = g
-        print("F,G:", f,g)
-        possiblePaths.append((f,"up"))
+        f = h+g
+        print("F,G,H :", f,g,h)
+        possiblePaths.append((f,"a_up"))
 
-    if "down" in possibleMoves:
+    if "b_down" in possibleMoves:
         copyCurDown = cur.copy()
         copyCurDown = swap("down",copyCurDown,ind0)
         print("Possible Move Down: ")
         printLs(copyCurDown)
-        # h = ham(initial, copyCurDown)
+        h = ham(initial, copyCurDown)
         g = ham(copyCurDown,goal)
-        f = g
-        print("F,G:", f,g)
-        possiblePaths.append((f,"down"))
+        f = h+g
+        print("F,G,H :", f,g,h)
+        possiblePaths.append((f,"b_down"))
     
-    if "left" in possibleMoves:
+    if "c_left" in possibleMoves:
         copyCurLeft = cur.copy()
         copyCurLeft = swap("left",copyCurLeft,ind0)
         print("Possible Move Left: ")
         printLs(copyCurLeft)
-        # h = ham(initial, copyCurLeft)
+        h = ham(initial, copyCurLeft)
         g = ham(copyCurLeft,goal)
-        f = g
-        print("F,G:", f,g)
-        possiblePaths.append((f,"left"))
+        f = h+g
+        print("F,G,H :", f,g,h)
+        possiblePaths.append((f,"c_left"))
 
-    if "right" in possibleMoves:
+    if "d_right" in possibleMoves:
         copyCurRight = cur.copy()
         copyCurRight = swap("right",copyCurRight,ind0)
         print("Possible Move Right: ")
         printLs(copyCurRight)
-        # h = ham(initial, copyCurRight)
+        h = ham(initial, copyCurRight)
         g = ham(copyCurRight,goal)
-        f = g
-        print("F,G:", f,g)
-        possiblePaths.append((f,"right"))
+        f = h+g
+        print("F,G,H :", f,g,h)
+        possiblePaths.append((f,"d_right"))
     
     print(f"possiblePaths and their values : {possiblePaths}")
     if (lastMove != ""):
-        if (lastMove in ["up","down"]):
-            notMove = "up" if lastMove == "down" else "down"
-        elif (lastMove in ["left","right"]):
-            notMove = "left" if lastMove == "right" else "right"
+        if (lastMove in ["a_up","b_down"]):
+            notMove = "a_up" if lastMove == "b_down" else "b_down"
+        elif (lastMove in ["c_left","d_right"]):
+            notMove = "left" if lastMove == "d_right" else "d_right"
         print("Last Move : ", lastMove)
         print("Not Move : ", notMove)
         print("Thus removing compliment of LastMove from possiblePaths...")
         for i in possiblePaths:
             if i[1] == notMove:
                 possiblePaths.remove(i)
-        print(f"possiblePaths and their values : {possiblePaths}")
+        print(f"possiblePaths and their values after removing lastmove: {possiblePaths}")
+    
+    # removing paths which are visited
+    for i in possiblePaths:
+        if (i in visited):
+            possiblePaths.remove(i)
+    print(f"possiblePaths and their values after Visited : {possiblePaths}")
 
     
     minPath = min(possiblePaths)
     print(f"Min : {minPath}")
-    if (minPath[1] == "up"):
-        return copyCurUp,"up"
-    if (minPath[1] == "down"):
-        return copyCurDown,"down"
-    if (minPath[1] == "left"):
-        return copyCurLeft,"left"
-    if (minPath[1] == "right"):
-        return copyCurRight,"right"
+    if (minPath[1] == "a_up"):
+        visited.append(copyCurUp)
+        return copyCurUp,"a_up"
+    if (minPath[1] == "b_down"):
+        visited.append(copyCurDown)
+        return copyCurDown,"b_down"
+    if (minPath[1] == "c_left"):
+        visited.append(copyCurLeft)
+        return copyCurLeft,"c_left"
+    if (minPath[1] == "d_right"):
+        visited.append(copyCurRight)
+        return copyCurRight,"d_right"
 
 
 def printLs(ls):
@@ -130,16 +150,23 @@ if __name__ == "__main__":
         7,8,0
     ]
     initial = [
-        1,2,3,
-        4,5,6,
-        0,7,8
+        1,8,2,
+        0,4,3,
+        7,6,5
     ]
 
     cur = initial
     lastMove = ""
     steps = 10
-    while (True):
-    # while (steps):
+    visited = [initial]
+    # while (True):
+    while (steps):
+        isSolvable = getInvCount(cur)
+        print(f"Is is solvable : {getInvCount(cur)}")
+        if (not isSolvable):
+            print(f"Is is solvable : {getInvCount(cur)}")
+            break
+        
         print("\n\n")
         print("CUR : ")
         printLs(cur)
--- tmp.py
+++ tmp.py
@@ -1,53 +1,70 @@
-def winningCaseOrRandomLoc():
-    remLoc = fetchRemLoc()
-    print(remLoc)
-    for tup in remLoc:
-        i,j = tup[0],tup[1]
-        print(i,j)
-        try:
-            # for dis in range(1,3):
-                # if (mat[i-dis][j-dis] == mat[i+dis][j+dis] == 1) or (mat[i-dis][j] == mat[i+dis][j] == 1) or (mat[i][j-dis] == mat[i][j+dis] == 1):
-                #     return i,j
+# def winningCaseOrRandomLoc():
+#     remLoc = fetchRemLoc()
+#     print(remLoc)
+#     for tup in remLoc:
+#         i,j = tup[0],tup[1]
+#         print(i,j)
+#         try:
+#             # for dis in range(1,3):
+#                 # if (mat[i-dis][j-dis] == mat[i+dis][j+dis] == 1) or (mat[i-dis][j] == mat[i+dis][j] == 1) or (mat[i][j-dis] == mat[i][j+dis] == 1):
+#                 #     return i,j
                 
-            if (mat[i-1][j-1] == mat[i+1][j+1] == 1):
-                print("in center backward slash")
-                return i,j
-            if (mat[i-1][j+1] == mat[i+1][j-1] == 1):
-                print("in center forward slash")
-                return i,j
-            if (mat[i-1][j] == mat[i+1][j] == 1):
-                print("in center vertical")
-                return i,j
-            if (mat[i][j-1] == mat[i][j+1] == 1):
-                print("in center horizontal")
-                return i,j
-            if (mat[i][j+1] == mat[i][j+2] == 1):
-                print("in left corner horizontal")
-                return i,j
-            if (mat[i][j-1] == mat[i][j-2] == 1):
-                print("in right corner horizontal")
-                return i,j
-            if (mat[i+1][j] == mat[i+2][j] == 1):
-                print("in up corner vertical")
-                return i,j
-            if (mat[i-1][j] == mat[i-2][j] == 1):
-                print("in down corner vertical")
-                return i,j
-        except IndexError:
-            pass
+#             if (mat[i-1][j-1] == mat[i+1][j+1] == 1):
+#                 print("in center backward slash")
+#                 return i,j
+#             if (mat[i-1][j+1] == mat[i+1][j-1] == 1):
+#                 print("in center forward slash")
+#                 return i,j
+#             if (mat[i-1][j] == mat[i+1][j] == 1):
+#                 print("in center vertical")
+#                 return i,j
+#             if (mat[i][j-1] == mat[i][j+1] == 1):
+#                 print("in center horizontal")
+#                 return i,j
+#             if (mat[i][j+1] == mat[i][j+2] == 1):
+#                 print("in left corner horizontal")
+#                 return i,j
+#             if (mat[i][j-1] == mat[i][j-2] == 1):
+#                 print("in right corner horizontal")
+#                 return i,j
+#             if (mat[i+1][j] == mat[i+2][j] == 1):
+#                 print("in up corner vertical")
+#                 return i,j
+#             if (mat[i-1][j] == mat[i-2][j] == 1):
+#                 print("in down corner vertical")
+#                 return i,j
+#         except IndexError:
+#             pass
 
-def fetchRemLoc():
-    remLoc = []
-    for row in range(len(mat)):
-        for ele in range(len(mat)):
-            if mat[row][ele] == -1:
-                remLoc.append((row, ele))
-    return remLoc
+# def fetchRemLoc():
+#     remLoc = []
+#     for row in range(len(mat)):
+#         for ele in range(len(mat)):
+#             if mat[row][ele] == -1:
+#                 remLoc.append((row, ele))
+#     return remLoc
 
-mat = [
-    [-1,-1,1],
-    [-1,-1,1],
-    [1,-1,-1]
+# mat = [
+#     [-1,-1,1],
+#     [-1,-1,1],
+#     [1,-1,-1]
+# ]
+
+# print("win : ",winningCaseOrRandomLoc())
+
+import pandas as pd
+
+df = pd.DataFrame([
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16],
+    [17,18,19,20],
+    [21,22,23,24],
+    [25,26,27,28],
+    [29,30,31,32]
 ]
+)
 
-print("win : ",winningCaseOrRandomLoc())
+print(df)
+print(df.iloc[[1,3,6]])
